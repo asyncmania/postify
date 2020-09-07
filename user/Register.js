9817687d48de7030710df212af7055fdb986adeb
jilesanmi <ilesanmi.josiah@gmail.com>
2020-09-08 00:35:17 +0100

    first version of postify

---
+++ user/Register.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+export default function Register({ setUser }) {
+  
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordRepeat, setPasswordRepeat] = useState('')
+
+  const handleUsername = (evt) => {
+    setUsername(evt.target.value)
+  }
+
+  function handlePassword (evt) {
+    setPassword(evt.target.value)
+}
+
+function handlePasswordRepeat (evt) {
+    setPasswordRepeat(evt.target.value)
+}
+
+  return (
+    <form onSubmit={(e) => { e.preventDefault(); setUser(username) }}>
+      <label htmlFor="register-username">Username:</label>
+      <input type="text" value={username} onChange={handleUsername} name="register-username" id="register-username" />
+      <label htmlFor="register-password">Password:</label>
+      <input type="password" value={password} onChange={handleUsername} name="register-password" id="register-password" />
+      <label htmlFor="register-password-repeat">Repeat password:</label>
+      <input
+        type="password"
+        value={passwordRepeat}
+        onChange={handlePasswordRepeat}
+        name="register-password-repeat"
+        id="register-password-repeat"
+      />
+      <input type="submit" value="Register" disabled={username.length === 0} />
+    </form>
+  );
+}
